779f28af8032752fdd8408cdcb2af5cf38ecb181
Dave12138 <35069593+Dave12138@users.noreply.github.com>
2019-06-15 22:13:44 +0800

    Update core.js

--- core.js
+++ core.js
@@ -32,12 +32,18 @@ function getref(){
     prompt('读取',getext)
     return getext
 }
-
+var ts = document.createElement('img')
+var bt = document.createElement('button')
 var btt = document.createElement('button')
+ts.src = 'https://img.moegirl.org/common/f/fc/%E5%B4%A9%E5%9D%8F3%E6%AC%A1%E5%85%83Q.png'
+bt.setAttribute('class','copybuttonein');
+bt.setAttribute('onclick','getlog()');
+bt.style = 'float:right;padding:10px 10px;z-index:999;position:absoluted;top:0px;left:-80px;display:block;';
+bt.appendChild(ts);
+var iaa=document.getElementsByClassName('history')[0]
+iaa.insertBefore(bt,iaa.firstChild);
 btt.setAttribute('onclick','getref()');
 btt.style = 'float:right;padding:10px 10px;z-index:999;position:absoluted;top:0px;left:-80px;display:block;';
-var tss = document.createElement('img')
-tss.src = 'https://img.moegirl.org/common/f/fc/%E5%B4%A9%E5%9D%8F3%E6%AC%A1%E5%85%83Q.png'
-btt.appendChild(tss);
+btt.appendChild(ts);
 var ibb=document.getElementsByClassName('remark')[0]
 ibb.insertBefore(btt,ibb.firstChild);
